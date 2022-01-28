922abd9feaca96bdb0bbe76f5ceb0971544a4780
mamccorkle <89654757+mamccorkle@users.noreply.github.com>
2022-01-28 11:29:01 -0600

    Initial commit with default project settings

---
+++ cmake-build-debug/CMakeFiles/Power_Play_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Power_Play_2.dir/main.cpp.obj"
+  "Power_Play_2.exe"
+  "Power_Play_2.exe.manifest"
+  "Power_Play_2.lib"
+  "Power_Play_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Power_Play_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
