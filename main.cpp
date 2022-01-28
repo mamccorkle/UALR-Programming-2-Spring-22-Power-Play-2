4ddbcaa42b748e3686454506c23258d7ab7a1f54
mamccorkle <89654757+mamccorkle@users.noreply.github.com>
2022-01-28 13:27:38 -0600

    Added instructions
    Added monsters.txt to the project
    
    Added function prototypes (as void to begin with)
    
    Added function definitions, post main
    
    Added function definition descriptions/objectives

--- main.cpp
+++ main.cpp
@@ -4,11 +4,63 @@
 //  Project: UALR - Programming 2 - Spring 22 - Power Play 2
 //  Created by: Mark McCorkle on 20220128
 //  Based on: Power Play 1 by Sean Orme
-//  IDE: CLion 2021.2.3     - VERIFIED/UNVERIFIED
+//  IDE: CLion 2021.2.3     - VERIFIED
 //  IDE: XCode              - VERIFIED/UNVERIFIED
 //  IDE: Visual Studio 2022 - VERIFIED/UNVERIFIED
 //  IDE: Linux via g++      - VERIFIED/UNVERIFIED
 //
+/*
+
+    For this PP we will be separating our code out into functions. We will also be changing up two pieces of
+    functionality:
+
+    1)  Damage dealt on attacks are based on a normal distribution with the mean being the object’s strength and a
+        standard deviation of 2.
+    2)  We are loading the monsters from a file. I have a “monsters.txt” file that looks like this:
+
+        3
+        Dragon
+        5
+        50
+        Imp
+        1
+        3
+        Orc
+        4
+        25
+
+    First line is the number of monsters then name, strength and health for each.
+
+    We will be dividing up the code into the following functions (NOTE:: the declarations should be at the top of the
+    file and the definitions at the end) (NOTE2:: You must decide return types and parameters!
+
+    This function loads the monsters from the passed in file and returns the vector. You’ll need a for loop.
+    loadMonsters();
+
+    Almost a copy and paste from PP1
+    displayBattle();
+
+    Almost a copy and paste from PP1
+    monsterAttack();
+
+    Almost a copy and paste from PP1
+    playerAttack();
+
+    returns damage done using normal distribution and the passed in object’s strength. I also output the object’s name,
+    along with “ deals “ mean: attacker.strength, stdev: 2.0
+    attack();
+
+    takes the defending object and the damage being done. subtracts the damage from the defender’s health. outputs:
+    damage << " damage to " << object.name << "!!!" << std::endl;
+    The code for a fight looks like this for the player attacking a monster:
+     defend(monsters[monsterNum - 1], attack(player));
+    defend();
+
+    uses a normal distribution with median of strength*2 and standard deviation of 3. adds random amount to object’s
+    health.
+    heal();
+
+ */
 #include <iostream>
 #include <string>
 #include <vector>
@@ -21,6 +73,15 @@ struct Object
     int strength{ 0 };
     int health{ 0 };
 };
+
+// Function Prototypes:
+void loadMonsters(  );
+void displayBattle(  );
+void monsterAttack(  );
+void playerAttack(  );
+void attack(  );
+void defend(  );
+void heal(  );
 
 int main()
 {
@@ -126,4 +187,51 @@ int main()
         std::cout << "You have killed the monsters!!!" << std::endl;
     }
     system("PAUSE");
+}
+
+// This function loads the monsters from the passed in file and returns the vector. You’ll need a for loop.
+void loadMonsters()
+{
+
+}
+
+// Almost a copy and paste from PP1
+void displayBattle()
+{
+
+}
+
+// Almost a copy and paste from PP1
+void monsterAttack()
+{
+
+}
+
+// Almost a copy and paste from PP1
+void playerAttack()
+{
+
+}
+
+// Returns damage done using normal distribution and the passed in object’s strength.
+//    I also output the object’s name, along with “ deals “ mean: attacker.strength, stdev: 2.0
+void attack()
+{
+
+}
+
+// Takes the defending object and the damage being done. subtracts the damage from the defender’s health. outputs:
+//    damage << " damage to " << object.name << "!!!" << std::endl;
+//    The code for a fight looks like this for the player attacking a monster:
+//     defend(monsters[monsterNum - 1], attack(player));
+void defend()
+{
+
+}
+
+// Uses a normal distribution with median of strength*2 and standard deviation of 3. adds random amount to object’s
+//    health.
+void heal()
+{
+
 }
